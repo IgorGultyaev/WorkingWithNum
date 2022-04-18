2ddb52b876722f85269a65ecfee1113721d56fe2
IgorGultyaev <igor.gultyaev@mail.ru>
2022-04-18 20:16:18 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static List<Integer> onlyPositive(List<Integer> intList) {
+        List<Integer> positiveList = new ArrayList<>();
+        for (Integer num : intList
+        ) {
+                if (num>0) {
+                    positiveList.add(num);
+                }
+        }
+        return positiveList;
+    }
+
+    static List<Integer> divisibleBy2(List<Integer> intList) {
+        List<Integer> positiveList = new ArrayList<>();
+        for (Integer num : intList
+        ) {
+                if (num%2==0) {
+                    positiveList.add(num);
+                }
+        }
+        return positiveList;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        intList = onlyPositive(intList);
+        intList = divisibleBy2(intList);
+        Collections.sort(intList);
+
+        intList.forEach(System.out::println);
+    }
+}
